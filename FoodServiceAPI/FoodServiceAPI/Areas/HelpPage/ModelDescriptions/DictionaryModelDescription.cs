2aa9cdb5e4e91be1d365d7f9a3312e8ca401e364
mceicys <31811836+mceicys@users.noreply.github.com>
2018-03-28 02:21:32 -0500

    Removed FoodServiceAPI Visual Studio project
    The project will be replaced with one that has less redundant generated
    code.

--- FoodServiceAPI/FoodServiceAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace FoodServiceAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- FoodServiceAPI/FoodServiceAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace FoodServiceAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
