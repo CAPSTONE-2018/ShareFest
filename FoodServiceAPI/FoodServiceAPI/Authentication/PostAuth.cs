6d1a2b738a4a072aa0527907ea83a0eba3c8750a
mceicys <31811836+mceicys@users.noreply.github.com>
2018-04-04 05:18:57 -0500

    Some api/user code and modified UserPass auth
    Implemented api/user/register, api/user/login, api/user/logout,
    api/user/getinfo, and api/user/logoutall. Also modified PostAuth.cs to
    get credentials from JSON in request's body instead of the POST form
    data.

--- FoodServiceAPI/FoodServiceAPI/Authentication/PostAuth.cs
+++ FoodServiceAPI/FoodServiceAPI/Authentication/PostAuth.cs
@@ -8,16 +8,19 @@ using Microsoft.AspNetCore.DataProtection;
 using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
+using System.Text;
 using System.Text.Encodings.Web;
 using System.Security.Claims;
+using System.IO;
+using Newtonsoft.Json;
 using FoodServiceAPI.Database;
 using FoodServiceAPI.Models;
 
 namespace FoodServiceAPI.Authentication
 {
     /*
-        Reads "username" and "password" POST parameters and checks against database.
-        Successful authentication gives User the "uid" and "username" claims.
+        Reads "username" and "password" parameters in POST body containing JSON and checks
+        against database. Successful authentication gives User the "uid" and "username" claims.
     */
     
     // FIXME: What should Fail messages contain? Should they influence what's sent to the client?
@@ -34,25 +37,39 @@ namespace FoodServiceAPI.Authentication
 
     public class PostAuthHandler : AuthenticationHandler<PostAuthOptions>
     {
+        public class Credentials
+        {
+            public string username { get; set; }
+            public string password { get; set; }
+        }
+
         public PostAuthHandler(IOptionsMonitor<PostAuthOptions> options, ILoggerFactory logger, UrlEncoder encoder, IDataProtectionProvider dataProtection, ISystemClock clock)
             : base(options, logger, encoder, clock)
         { }
 
         protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
         {
-            // Request must contain POST form with username and password parameters
-            if(!Request.HasFormContentType || Request.Method != "POST" ||
-            !Request.Form.ContainsKey("username") || !Request.Form.ContainsKey("password"))
+            // Request must be POST containg JSON
+            if(Request.Method != "POST" || Request.ContentType != "application/json")
                 return AuthenticateResult.NoResult();
+
+            // Parse body JSON for credentials
+            Credentials cred;
+
+            using (StreamReader reader = new StreamReader(Request.Body, Encoding.UTF8))
+            {
+                string body = await reader.ReadToEndAsync();
+                cred = JsonConvert.DeserializeObject(body, typeof(Credentials)) as Credentials;
+            }
 
             // Get user entity
             FoodContext dbContext = Context.RequestServices.GetService(typeof(FoodContext)) as FoodContext;
-            UserData user = await dbContext.Users.FirstOrDefaultAsync(u => u.username == Request.Form["username"]);
+            UserData user = await dbContext.Users.FirstOrDefaultAsync(u => u.username == cred.username);
 
             // Check password
             PasswordProtector protector = new PasswordProtector();
 
-            if(!protector.Compare(Request.Form["password"], user.password))
+            if(!protector.Compare(cred.password, user.password))
                 return AuthenticateResult.Fail("incorrect password");
 
             // Authenticated; create identity
--- FoodServiceAPI/FoodServiceAPI/Controllers/UserController.cs
+++ FoodServiceAPI/FoodServiceAPI/Controllers/UserController.cs
@@ -23,28 +23,121 @@ namespace FoodServiceAPI.Controllers
     {
         private readonly FoodContext dbContext;
 
+        public class UserRegistration
+        {
+            public string username { get; set; }
+            public string password { get; set; }
+            public string email { get; set; }
+            public string address { get; set; }
+            public string zip { get; set; }
+            public string user_type { get; set; }
+
+            // Client
+            public string first_name { get; set; }
+            public string last_name { get; set; }
+            public string cell_phone { get; set; }
+            public bool paying { get; set; }
+
+            // Business
+            public string name { get; set; }
+            public string work_phone { get; set; }
+            public string instructions { get; set; }
+        }
+
+        public class UserInfo
+        {
+            public string username { get; set; }
+            public string email { get; set; }
+            public string address { get; set; }
+            public string zip { get; set; }
+
+            public UserInfo(UserData user)
+            {
+                username = user.username;
+                email = user.email;
+                address = user.address;
+                zip = user.zip;
+            }
+        }
+
+        public class ClientInfo : UserInfo
+        {
+	        public string first_name { get; set; }
+	        public string last_name { get; set; }
+	        public string cell_phone { get; set; }
+
+            public ClientInfo(Client client) : base(client.User)
+            {
+                first_name = client.first_name;
+                last_name = client.last_name;
+                cell_phone = client.cell_phone;
+            }
+        }
+
+        public class BusinessInfo : UserInfo
+        {
+            public string name { get; set; }
+            public string work_phone { get; set; }
+            public string instructions { get; set; }
+
+            public BusinessInfo(Business business) : base(business.User)
+            {
+                name = business.name;
+                work_phone = business.work_phone;
+                instructions = business.instructions;
+            }
+        }
+
         public UserController(FoodContext dbContext)
         {
             this.dbContext = dbContext;
         }
 
-        // FIXME: This creates a placeholder user without validation; implement full registration
-        // FIXME: JSON input
         [Route("register")]
         [HttpPost]
         [AllowAnonymous]
-        public async Task<string> Register(string username, string password)
+        public async Task<string> Register([FromBody]UserRegistration reg)
         {
+            // FIXME: validate
             UserData user = new UserData
             {
-                username = username,
-                password = new PasswordProtector().Protect(password),
-                email = "no_email",
-                address = "no_address",
-                zip = "no_zip"
+                username = reg.username,
+                password = new PasswordProtector().Protect(reg.password),
+                email = reg.email,
+                address = reg.address,
+                zip = reg.zip
             };
 
             await dbContext.Users.AddAsync(user);
+
+            if(reg.user_type.ToLower() == "client")
+            {
+                Client client = new Client
+                {
+                    uid = user.uid,
+                    first_name = reg.first_name,
+                    last_name = reg.last_name,
+                    cell_phone = reg.cell_phone,
+                    paying = reg.paying
+                };
+
+                await dbContext.Clients.AddAsync(client);
+            }
+            else if(reg.user_type.ToLower() == "business")
+            {
+                Business business = new Business
+                {
+                    uid = user.uid,
+                    name = reg.name,
+                    work_phone = reg.work_phone,
+                    instructions = reg.instructions
+                };
+
+                await dbContext.Businesses.AddAsync(business);
+            }
+            else
+                return "INVALID_USER_TYPE"; // FIXME: Standard acknowledge
+
             await dbContext.SaveChangesAsync();
 
             return "OK"; // FIXME: Standard acknowledge
@@ -92,17 +185,28 @@ namespace FoodServiceAPI.Controllers
         [Authorize("Session")]
         public async Task<string> Logout()
         {
-            throw new NotImplementedException(); // FIXME
+            SessionData session = await dbContext.Sessions.FindAsync(Convert.ToInt32(User.FindFirstValue("sid")));
+            dbContext.Sessions.Remove(session);
+            await dbContext.SaveChangesAsync();
+
+            return "OK"; // FIXME: Standard acknowledge
         }
 
-        // FIXME: This placeholder returns username; implement actual getinfo
         [Route("getinfo")]
         [HttpPost]
         [Authorize("Session")]
-        public async Task<string> GetInfo()
+        public async Task<JsonResult> GetInfo()
         {
-            UserData user = await dbContext.Users.FindAsync(Convert.ToInt32(User.FindFirstValue("uid")));
-            return user.username;
+            int uid = Convert.ToInt32(User.FindFirstValue("uid"));
+            UserData user = await dbContext.Users.Include(u => u.Client).Include(u => u.Business).FirstOrDefaultAsync(u => u.uid == uid);
+
+            // FIXME: Standard acknowledge
+            if (user.Client != null)
+                return Json(new ClientInfo(user.Client));
+            else if (user.Business != null)
+                return Json(new BusinessInfo(user.Business));
+            else
+                return Json("OTHER");
         }
 
         [Route("logoutall")]
@@ -110,7 +214,12 @@ namespace FoodServiceAPI.Controllers
         [Authorize("UserPass")]
         public async Task<string> LogoutAllSessions()
         {
-            throw new NotImplementedException(); // FIXME
+            int uid = Convert.ToInt32(User.FindFirstValue("uid"));
+            SessionData[] sessions = await dbContext.Sessions.Where(s => s.uid == uid).ToArrayAsync();
+            dbContext.Sessions.RemoveRange(sessions);
+            await dbContext.SaveChangesAsync();
+
+            return "OK"; // FIXME: Standard acknowledge
         }
 
         [Route("setinfo")]
