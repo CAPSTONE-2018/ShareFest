6022b2958ce766386b7365f7af6b1d1750202c46
fhuda <farminahuda@lewisu.edu>
2018-04-06 11:24:31 -0500

    Farmina - added registration page

--- FoodServiceMobileApp/app/src/main/java/com/example/jaren/foodservice_mobileapp/LoginActivity.java
+++ FoodServiceMobileApp/app/src/main/java/com/example/jaren/foodservice_mobileapp/LoginActivity.java
@@ -14,6 +14,8 @@ public class LoginActivity extends AppCompatActivity {
     private TextView Info;
     private Button Login;
     private Button Register;
+
+
     private int NoOfAttempts = 5;
 
     @Override
@@ -21,11 +23,13 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Name = (EditText)findViewById(R.id.etName);
+        Name = (EditText) findViewById(R.id.etName);
         Password = (EditText) findViewById(R.id.etPassword);
         Info = (TextView) findViewById(R.id.tvInfo);
-        Login = (Button)findViewById(R.id.btnLogin);
-        Register = (Button)findViewById(R.id.btnRegister);
+        Login = (Button) findViewById(R.id.btnLogin);
+        Register = (Button) findViewById(R.id.btnRegister);
+
+
 
         Info.setText("No of attempts remaining: " + NoOfAttempts);
 
@@ -36,14 +40,29 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        Register.setOnClickListener(new View.OnClickListener(){
+        Register.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
                 Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
                 startActivity(registerIntent);
             }
         });
+
+        //that causes the problem....
+        //==============================================================
+
+//        BusinessRegister.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent BusinessRegisterIntent = new Intent(LoginActivity.this, RegisterBusinessActivity.class);
+//                startActivity(BusinessRegisterIntent);
+//            }
+//        });
+
+        //==============================================================
+
     }
+
 
     private void ValidateLogin(String userName, String userPassword){
         if ((userName.equals("Admin")) && (userPassword.equals("1234"))){
--- FoodServiceMobileApp/app/src/main/java/com/example/jaren/foodservice_mobileapp/RegisterActivity.java
+++ FoodServiceMobileApp/app/src/main/java/com/example/jaren/foodservice_mobileapp/RegisterActivity.java
@@ -1,13 +1,54 @@
 package com.example.jaren.foodservice_mobileapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private Button BusinessRegister;
+    private RadioGroup RG;
+    private RadioButton RBBusiness;
+    private RadioButton RBClient;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        BusinessRegister = (Button) findViewById(R.id.btnNext);
+        RG = (RadioGroup) findViewById(R.id.radioGroupRegister);
+        RBBusiness = (RadioButton) findViewById(R.id.rbtnBusiness);
+        RBClient = (RadioButton) findViewById(R.id.rbtnClient);
+
+
+        BusinessRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (RBBusiness.isChecked()) {
+                    Intent registerBusinessIntent = new Intent(RegisterActivity.this, RegisterBusinessActivity.class);
+                    startActivity(registerBusinessIntent);
+                }
+
+                if (RBClient.isChecked())
+                {Intent registerClientIntent = new Intent(RegisterActivity.this, RegisterClientActivity.class);
+                    startActivity(registerClientIntent);
+
+                }
+
+
+
+            }
+        });
+
     }
+
+
 }
