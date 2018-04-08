0b7ee7080fbdb3b6edd89b13f4727d0314dbc7e7
Nadeem <Nadeem@DESKTOP-T5RRKD6>
2018-04-08 12:14:57 -0400

    New solution created for web client using entity framerwork scaffolding

---
+++ CapstoneProj/CapstoneProj/Controllers/ClientViewModelsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CapstoneProj.Models;
+
+namespace CapstoneProj.Controllers
+{
+    public class ClientViewModelsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: ClientViewModels
+        public ActionResult Index()
+        {
+            return View(db.ClientViewModels.ToList());
+        }
+
+        // GET: ClientViewModels/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ClientViewModel clientViewModel = db.ClientViewModels.Find(id);
+            if (clientViewModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clientViewModel);
+        }
+
+        // GET: ClientViewModels/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ClientViewModels/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,first_name,last_name,Username,Address,zip,Email,PhoneNumber,Password,ConfirmPassword,pay")] ClientViewModel clientViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ClientViewModels.Add(clientViewModel);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(clientViewModel);
+        }
+
+        // GET: ClientViewModels/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ClientViewModel clientViewModel = db.ClientViewModels.Find(id);
+            if (clientViewModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clientViewModel);
+        }
+
+        // POST: ClientViewModels/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,first_name,last_name,Username,Address,zip,Email,PhoneNumber,Password,ConfirmPassword,pay")] ClientViewModel clientViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(clientViewModel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(clientViewModel);
+        }
+
+        // GET: ClientViewModels/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ClientViewModel clientViewModel = db.ClientViewModels.Find(id);
+            if (clientViewModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clientViewModel);
+        }
+
+        // POST: ClientViewModels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ClientViewModel clientViewModel = db.ClientViewModels.Find(id);
+            db.ClientViewModels.Remove(clientViewModel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CapstoneProj/CapstoneProj/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CapstoneProj.MvcApplication" Language="C#" %>
