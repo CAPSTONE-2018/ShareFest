43ab6a329ee1f785280d273d12e5f48d5dcf5fb6
Jann Rene Ibay <jannibay@yahoo.com>
2018-05-06 11:19:02 -0500

    Updated listview

--- FoodServiceMobileApp/app/src/main/java/com/example/jaren/foodservice_mobileapp/BusinessClaimablePackagesActivity.java
+++ FoodServiceMobileApp/app/src/main/java/com/example/jaren/foodservice_mobileapp/BusinessClaimablePackagesActivity.java
@@ -2,12 +2,45 @@ package com.example.jaren.foodservice_mobileapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
 public class BusinessClaimablePackagesActivity extends AppCompatActivity {
+
+    private ListView PackageList;
+
+    private String[] SamplePackages = {"Marianos Package", "Jewel Osco Package", "Sam's Grocer Package", "Oriental Foods Package"};
+    private String[] SampleDescriptions = {"Salads", "Breads", "Fruits", "Candy"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_business_claimable_packages);
+
+        PackageList = (ListView)findViewById(R.id.lvPackageList);
+    }
+
+    class CustomAdapter extends BaseAdapter{
+        @Override
+        public int getCount() {
+            return SamplePackages.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return null;
+        }
     }
 }
