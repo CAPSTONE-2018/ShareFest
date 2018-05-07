d2d3b7292e492e1d642b8d4aa0e914e92d5cf4d1
mceicys <31811836+mceicys@users.noreply.github.com>
2018-05-07 00:37:17 -0500

    Create package from mobile app

--- FoodServiceMobileApp/app/src/main/java/com/example/jaren/foodservice_mobileapp/BusinessCreatePackageActivity.java
+++ FoodServiceMobileApp/app/src/main/java/com/example/jaren/foodservice_mobileapp/BusinessCreatePackageActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BusinessCreatePackageActivity extends AppCompatActivity {
 
@@ -16,6 +19,8 @@ public class BusinessCreatePackageActivity extends AppCompatActivity {
     private EditText PackageQuantity;
     private EditText PackagePrice;
     private EditText PackageExpiry;
+
+    private String urlCreatePackage = "http://10.0.2.2:50576/api/package/createpackage";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +49,59 @@ public class BusinessCreatePackageActivity extends AppCompatActivity {
             public void onClick(View view) {
                 // Send Package
                 CreatePackage.setEnabled(false);
+
+                Map<String, String> params = new HashMap<>();
+                params.put("name", PackageName.getText().toString());
+                putIfNotEmpty(params, "description", PackageDescription.getText().toString());
+
+                /* FIXME: The database doesn't treat quantity as a number (e.g. business might want
+                to specify units), so PackageQuantity should probably not be limited to integers
+                only */
+                putIfNotEmpty(params, "quantity", PackageQuantity.getText().toString());
+
+                params.put("price", PackagePrice.getText().toString());
+
+                // FIXME: Limit PackageExpiry to date format
+                // FIXME: Convert local time to UTC
+                putIfNotEmpty(params, "expires", PackageExpiry.getText().toString());
+
+                String token = getSharedPreferences("food_service_session", MODE_PRIVATE).
+                        getString("token", "");
+
+                HttpPostAsyncTask request = new HttpPostAsyncTask(params, new CreatePackageCallback(), token);
+                request.execute(urlCreatePackage);
             }
         });
+    }
+
+    private String putIfNotEmpty(Map<String, String> map, String key, String value) {
+        // FIXME: Ignore white space
+        if(!value.equals("")) {
+            return map.put(key, value);
+        }
+
+        return null;
+    }
+
+    private class CreatePackageCallback implements HttpPostAsyncTask.Callback {
+        public void onPostExecute(HttpPostCallbackResult result) {
+            String msg;
+
+            if(result.statusCode == 200) {
+                msg = "Created package.";
+
+                PackageName.getText().clear();
+                PackageDescription.getText().clear();
+                PackageQuantity.getText().clear();
+                PackagePrice.getText().clear();
+                PackageExpiry.getText().clear();
+            } else {
+                // FIXME: Make user-friendly
+                msg = "Creation failed (" + Integer.toString(result.statusCode) + ")";
+            }
+
+            Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+            CreatePackage.setEnabled(true);
+        }
     }
 }
